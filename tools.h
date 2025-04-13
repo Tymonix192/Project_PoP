6a4f0b23f8e6f92dba5a9dab3f3ae3e391cd026f
tymonix192 <karol.wickel@gmail.com>
2025-04-13 15:39:22 +0200

    draw functions for circle and point, as well as graphic module

--- tools.h
+++ tools.h
@@ -3,6 +3,8 @@
 
 #include <vector>
 #include <iostream>
+#include "graphic_gui.h"
+
 
 constexpr float EPSIL_ZERO = 0.0;
 
@@ -20,6 +22,8 @@ class Point{
         virtual int set_center(S2d coordinates);
         virtual S2d get_center();
         double distance_org(); // returns distance from origin
+        virutal void draw(Color color);
+        int set_coordinates(S2d coordinates);
     private:
         S2d _point;
         
@@ -36,7 +40,8 @@ class Circle: public Point{
         bool check_intersect(Circle other_circle);
         int set_center(S2d coordinates);
         S2d get_center();
-        private:
+        void draw(Color color);
+    private:
         double _radius;
         S2d _center;
 };
